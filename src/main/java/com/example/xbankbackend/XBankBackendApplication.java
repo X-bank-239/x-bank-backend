8aae9b067bd6faeb618bd3628ad5442380d9a6bd
Alex Sergeev <88105767+alwwwd@users.noreply.github.com>
2025-10-07 16:52:52 +0300

    Init

---
+++ src/main/java/com/example/xbankbackend/XBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.xbankbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XBankBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/xbankbackend/XBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xbankbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
