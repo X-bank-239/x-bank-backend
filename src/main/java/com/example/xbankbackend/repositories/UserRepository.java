48ecfe87f498775003e7c6de03aa7f04211cd0c3
mileZ239 <misalazarev42@gmail.com>
2025-10-31 13:03:36 +0300

    Convenient access to DB

--- src/main/java/com/example/xbankbackend/repositories/UserRepository.java
+++ src/main/java/com/example/xbankbackend/repositories/UserRepository.java
@@ -1,6 +1,5 @@
 package com.example.xbankbackend.repositories;
 
-import com.example.xbankbackend.generated.tables.Users;
 import com.example.xbankbackend.models.User;
 import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,8 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Objects;
 import java.util.UUID;
+
+import static com.example.xbankbackend.generated.Tables.USERS;
 
 @Repository
 public class UserRepository {
@@ -19,30 +20,30 @@ public class UserRepository {
     }
 
     public void createUser(User user) {
-        dsl.insertInto(Users.USERS)
+        dsl.insertInto(USERS)
                 .values(user.getUserId(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getBirthdate())
                 .execute();
     }
 
     public User getUser(UUID uuid) {
         return Objects.requireNonNull(
-                dsl.selectFrom(Users.USERS)
-                        .where(Users.USERS.USER_ID.eq(uuid))
+                dsl.selectFrom(USERS)
+                        .where(USERS.USER_ID.eq(uuid))
                         .fetchOne()
                 )
                 .into(User.class);
     }
 
     public boolean haveEmail(String email) {
-        return dsl.selectFrom(Users.USERS)
-                .where(Users.USERS.EMAIL.eq(email))
+        return dsl.selectFrom(USERS)
+                .where(USERS.EMAIL.eq(email))
                 .fetch()
                 .size() == 1;
     }
 
     public boolean haveUUID(UUID uuid) {
-        return dsl.selectFrom(Users.USERS)
-                .where(Users.USERS.USER_ID.eq(uuid))
+        return dsl.selectFrom(USERS)
+                .where(USERS.USER_ID.eq(uuid))
                 .fetch()
                 .size() == 1;
     }
