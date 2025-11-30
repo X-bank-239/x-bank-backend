4826fba5af55afd8682e40eec298bf645321da7f
Alex Sergeev <88105767+alwwwd@users.noreply.github.com>
2025-11-30 23:05:01 +0300

    Revert "Mapstruct"

--- src/main/java/com/example/xbankbackend/controllers/UserController.java
+++ src/main/java/com/example/xbankbackend/controllers/UserController.java
@@ -2,37 +2,33 @@ package com.example.xbankbackend.controllers;
 
 import com.example.xbankbackend.dtos.requests.CreateUserRequest;
 import com.example.xbankbackend.dtos.responses.UserProfileResponse;
-import com.example.xbankbackend.mappers.UserMapper;
 import com.example.xbankbackend.models.User;
 import com.example.xbankbackend.services.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@AllArgsConstructor
 @Log4j2
-@CrossOrigin
 @RestController
-@RequiredArgsConstructor
+@CrossOrigin
 @RequestMapping("/user")
 public class UserController {
 
     private UserService userService;
 
-    private UserMapper userMapper;
-
     @PostMapping("/create")
-    public ResponseEntity<User> create( @RequestBody CreateUserRequest userRequest) {
+    public ResponseEntity<User> create(@Valid @RequestBody CreateUserRequest userRequest) {
         log.info("Creating user: {}", userRequest);
-        User user = userMapper.requestToAccount(userRequest);
-        User createdUser = userService.create(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+
+        User user = convertRequest(userRequest);
+        userService.create(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
     @GetMapping("/get-profile/{userId}")
@@ -40,5 +36,16 @@ public class UserController {
         log.info("Getting user: {}", userId);
         UserProfileResponse userProfile = userService.getProfile(userId);
         return ResponseEntity.ok(userProfile);
+    }
+
+    private User convertRequest(CreateUserRequest userRequest) {
+        User user = new User();
+
+        user.setFirstName(userRequest.getFirstName());
+        user.setLastName(userRequest.getLastName());
+        user.setEmail(userRequest.getEmail());
+        user.setBirthdate(userRequest.getBirthdate());
+
+        return user;
     }
 }
--- src/main/java/com/example/xbankbackend/repositories/UserRepository.java
+++ src/main/java/com/example/xbankbackend/repositories/UserRepository.java
@@ -2,26 +2,23 @@ package com.example.xbankbackend.repositories;
 
 import com.example.xbankbackend.models.User;
 import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 import static com.example.xbankbackend.generated.Tables.USERS;
-@Log4j2
+
 @AllArgsConstructor
 @Repository
 public class UserRepository {
 
     private final DSLContext dsl;
 
-    public Optional<User> create(User user) {
-            dsl.insertInto(USERS)
+    public void create(User user) {
+        dsl.insertInto(USERS)
                 .values(user.getUserId(), user.getFirstName(), user.getLastName(), user.getEmail(), user.getBirthdate())
                 .execute();
-            return Optional.ofNullable(this.getUser(user.getUserId()));
     }
 
     public User getUser(UUID userId) {
