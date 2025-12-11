16b887de6fbed45dd6382e4491a8fe8a9274856d
Alex Sergeev <88105767+alwwwd@users.noreply.github.com>
2025-12-11 22:40:40 +0300

    Map user profile

--- src/main/java/com/example/xbankbackend/dtos/responses/UserProfileResponse.java
+++ src/main/java/com/example/xbankbackend/dtos/responses/UserProfileResponse.java
@@ -1,11 +1,15 @@
 package com.example.xbankbackend.dtos.responses;
 
-import lombok.Data;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserProfileResponse {
     private String firstName;
     private String lastName;
---
+++ src/main/java/com/example/xbankbackend/mappers/UserProfileMapper.java
@@ -0,0 +1,31 @@
+package com.example.xbankbackend.mappers;
+
+import com.example.xbankbackend.dtos.responses.BankAccountResponse;
+import com.example.xbankbackend.dtos.responses.UserProfileResponse;
+import com.example.xbankbackend.models.BankAccount;
+import com.example.xbankbackend.models.User;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UserProfileMapper {
+    public UserProfileResponse map(User user, List<BankAccount> account){
+        UserProfileResponse userProfileResponse = new UserProfileResponse().builder()
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .birthdate(user.getBirthdate())
+                .accounts(account.stream().map(
+                        bankAccount -> {
+                            BankAccountResponse bankAccountResponse = new BankAccountResponse();
+                            bankAccountResponse.setAmount(bankAccount.getBalance());
+                            bankAccountResponse.setCurrency(bankAccount.getCurrency());
+                            bankAccountResponse.setAccountType(bankAccount.getAccountType());
+                            return bankAccountResponse;
+                        }
+                ).toList())
+                .build();
+        return userProfileResponse;
+    }
+}
