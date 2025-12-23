968408dcd1e1e8a18ae1d5ec92c92858a04ae47a
Alex Sergeev <88105767+alwwwd@users.noreply.github.com>
2025-12-24 00:02:30 +0300

    adding new fields

--- src/main/java/com/example/xbankbackend/dtos/responses/UserProfileResponse.java
+++ src/main/java/com/example/xbankbackend/dtos/responses/UserProfileResponse.java
@@ -1,16 +1,23 @@
 package com.example.xbankbackend.dtos.responses;
 
-import lombok.*;
-import lombok.experimental.SuperBuilder;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserProfileResponse {
+    private UUID userId;
     private String firstName;
     private String lastName;
     private String email;
