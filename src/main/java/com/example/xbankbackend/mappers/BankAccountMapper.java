76ddd1f622d212217e8905bd1c5e9ada96617a55
Alex Sergeev <88105767+alwwwd@users.noreply.github.com>
2025-12-23 23:18:25 +0300

    JWT and hashing deps

--- src/main/java/com/example/xbankbackend/mappers/BankAccountMapper.java
+++ src/main/java/com/example/xbankbackend/mappers/BankAccountMapper.java
@@ -2,9 +2,7 @@ package com.example.xbankbackend.mappers;
 
 import com.example.xbankbackend.dtos.requests.CreateBankAccountRequest;
 import com.example.xbankbackend.models.BankAccount;
-import com.fasterxml.jackson.databind.util.Converter;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface BankAccountMapper {
