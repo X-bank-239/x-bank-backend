4d6b046ffb59fb49633bf43330497ac075270ad5
Alex Sergeev <88105767+alwwwd@users.noreply.github.com>
2025-12-23 23:47:54 +0300

    Snake case for all models

--- src/main/java/com/example/xbankbackend/dtos/responses/BankAccountResponse.java
+++ src/main/java/com/example/xbankbackend/dtos/responses/BankAccountResponse.java
@@ -2,11 +2,14 @@ package com.example.xbankbackend.dtos.responses;
 
 import com.example.xbankbackend.enums.BankAccountType;
 import com.example.xbankbackend.enums.CurrencyType;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class BankAccountResponse {
     private BigDecimal amount;
     private CurrencyType currency;
