e7a26a25dacaba77d34c9b0a121c2f2b5d9d0e8f
Alex Sergeev <88105767+alwwwd@users.noreply.github.com>
2025-12-30 22:20:39 +0300

    User logining

--- src/main/java/com/example/xbankbackend/controllers/UserController.java
+++ src/main/java/com/example/xbankbackend/controllers/UserController.java
@@ -5,7 +5,6 @@ import com.example.xbankbackend.dtos.requests.CreateUserRequest;
 import com.example.xbankbackend.dtos.responses.UserProfileResponse;
 import com.example.xbankbackend.jwt.JwtUtil;
 import com.example.xbankbackend.mappers.UserMapper;
-import com.example.xbankbackend.mappers.UserProfileMapper;
 import com.example.xbankbackend.models.User;
 import com.example.xbankbackend.services.UserService;
 import lombok.AllArgsConstructor;
@@ -26,7 +25,6 @@ public class UserController {
     private UserService userService;
     private UserMapper userMapper;
     private JwtUtil jwtUtil;
-    private UserProfileMapper userProfileMapper;
 
     @PostMapping("/create")
     public ResponseEntity<UserProfileResponse> create(@RequestBody CreateUserRequest userRequest) {
@@ -45,15 +43,15 @@ public class UserController {
     }
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthUserRequest user) {
-        log.info("Logining user: {}", user.getEmail());
+        log.info("Logging user: {}", user.getEmail());
         if (userService.authenticated(user.getEmail(), user.getPassword())) {
             String token = userService.generateTokenByEmail(user.getEmail());
             return ResponseEntity
-                    .status(HttpStatus.CREATED)
+                    .status(HttpStatus.OK)
                     .header("Authorization", "Bearer " + token)
                     .build();
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
     @GetMapping("/get-profile/{userId}")
