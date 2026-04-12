da40396056a17d90a78e9b8565248bcfae8e45c9
alwwwd <88105767+alwwwd@users.noreply.github.com>
2026-04-12 17:59:16 +0300

    migration fix

--- src/main/resources/db/migration/V11__loan_service_bank_account.sql
+++ src/main/resources/db/migration/V11__loan_service_bank_account.sql
@@ -10,21 +10,4 @@ VALUES ('00000000-0000-4000-8000-000000000002'::uuid,
         0.00,
         'RUB',
         'DEBIT');
-INSERT INTO bank_accounts (account_id, user_id, balance, currency, account_type)
-VALUES ('00000000-0000-4000-8000-000000000003'::uuid,
-        '00000000-0000-4000-8000-000000000001'::uuid,
-        0.00,
-        'USD',
-        'DEBIT');
-INSERT INTO bank_accounts (account_id, user_id, balance, currency, account_type)
-VALUES ('00000000-0000-4000-8000-000000000004'::uuid,
-        '00000000-0000-4000-8000-000000000001'::uuid,
-        0.00,
-        'EUR',
-        'DEBIT');
-INSERT INTO bank_accounts (account_id, user_id, balance, currency, account_type)
-VALUES ('00000000-0000-4000-8000-000000000005'::uuid,
-        '00000000-0000-4000-8000-000000000001'::uuid,
-        0.00,
-        'CNY',
-        'DEBIT');
+
