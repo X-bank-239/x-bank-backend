abf630165b123f1b0e3796262e9e3f620fce7144
alwwwd <88105767+alwwwd@users.noreply.github.com>
2026-04-12 17:50:14 +0300

    Updated constructor

--- src/main/java/com/example/xbankbackend/models/Transaction.java
+++ src/main/java/com/example/xbankbackend/models/Transaction.java
@@ -5,20 +5,26 @@ import com.example.xbankbackend.enums.TransactionStatus;
 import com.example.xbankbackend.enums.TransactionType;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Transaction {
 
     @NotNull(message = "transactionType cannot be null")
     private TransactionType transactionType;
-
     private UUID transactionId;
     private UUID senderId;
     private UUID receiverId;
