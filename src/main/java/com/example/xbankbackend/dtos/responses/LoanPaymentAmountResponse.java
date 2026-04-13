4e97d165dda6d6afc23aadebca22673b67d60bf8
alwwwd <88105767+alwwwd@users.noreply.github.com>
2026-04-13 22:50:00 +0300

    amount response

---
+++ src/main/java/com/example/xbankbackend/dtos/responses/LoanPaymentAmountResponse.java
@@ -0,0 +1,23 @@
+package com.example.xbankbackend.dtos.responses;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoanPaymentAmountResponse {
+    @NotNull(message = "amount cannot be null")
+    @Positive(message = "amount must be positive")
+    private BigDecimal amount;
+}
