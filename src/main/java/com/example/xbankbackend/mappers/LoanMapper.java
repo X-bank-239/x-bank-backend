b126d2014bbda6f02fd9f053b9cd4145d392f695
alwwwd <88105767+alwwwd@users.noreply.github.com>
2026-04-13 22:49:32 +0300

    Mapper update

--- src/main/java/com/example/xbankbackend/mappers/LoanMapper.java
+++ src/main/java/com/example/xbankbackend/mappers/LoanMapper.java
@@ -4,8 +4,10 @@ import com.example.xbankbackend.dtos.responses.LoanResponse;
 import com.example.xbankbackend.models.Loan;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface LoanMapper {
     LoanResponse loanToResponse(Loan loan);
-
+    List<LoanResponse> loansToResponses(List<Loan> loans);
 }
